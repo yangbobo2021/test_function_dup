712c9dfc10ad17a7f551528d923ddca521652f57
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-07-21 10:30:24 +0800

    Create test3.c

---
+++ test3.c
@@ -0,0 +1,35 @@
+void fun31() {
+  int a = 20;
+  int b = 20;
+  int c = a+b;
+  for (; a<100; a++) {
+    printf(c);
+  }
+}
+
+void fun32() {
+  int a = 30;
+  int b = 20;
+  int c = a+b;
+  for (; a<100; a++) {
+    printf(c);
+  }
+}
+
+void fun33() {
+  int a = 20;
+  int c = a+b;
+  for (; a<100; a++) {
+    printf(c);
+  }
+}
+
+void fun34() {
+  int a = 20;
+  int a2 = 20;
+  int b = 20;
+  int c = a+b;
+  for (; a<100; a++) {
+    printf(c);
+  }
+}
