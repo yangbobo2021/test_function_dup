e44a875c8eb16daf5ce7744999046d295adfa909
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-07-21 12:03:45 +0800

    Create test1_v3.py

---
+++ test1_v3.py
@@ -0,0 +1,41 @@
+
+def Test21():
+    a = 20
+    b = 30
+    for i in range(1, 30):
+        a = a+b
+    
+    b = a-b
+    print(b)
+    
+ def Test22():
+    a = 30
+    b = 30
+    for i in range(1, 30):
+        a = a+b
+    
+    b = a-b
+    print(b)
+    
+def Test23():
+    a = 20
+    for i in range(1, 30):
+        a = a+b
+    
+    b = a-b
+    print(b)
+    
+    
+def Test24():
+    a = 20
+    a2 = 40
+    b = 30
+    for i in range(1, 30):
+        a = a+b
+    
+    b = a-b
+    print(b)
+    
+def Test25():
+  a = 20
+  Test2()
