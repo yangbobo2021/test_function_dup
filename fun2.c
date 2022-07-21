e5f5f48690b4986e4f5d9e35514ce225f2b68ad6
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-07-21 10:06:06 +0800

    Create fun2.c

---
+++ fun2.c
@@ -0,0 +1,18 @@
+
+
+void fun1() {
+  int a = 20;
+  int b = 20;
+  int c = a+b;
+  for (; a<100; a++) {
+    printf(c);
+  }
+}
+
+void fun2() {
+  int a = 20;
+  int c;
+  for (; a<100; a++) {
+    printf(c);
+  }
+}
